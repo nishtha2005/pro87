444c95a24271212b966690e00e20e44075bbf2a5
nishtha2005 <80937422+nishtha2005@users.noreply.github.com>
2022-07-07 19:53:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pro87
+# picgram
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-MuVKB0G9w3MLCs0xOda13TyvAO_UjYI",
+  authDomain: "storytelling-f4a0e.firebaseapp.com",
+  databaseURL: "https://storytelling-f4a0e-default-rtdb.firebaseio.com",
+  projectId: "storytelling-f4a0e",
+  storageBucket: "storytelling-f4a0e.appspot.com",
+  messagingSenderId: "20087801271",
+  appId: "1:20087801271:web:393966ada6d458ce70d432"
+};
